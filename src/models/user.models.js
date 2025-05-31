a143ddf8001155e04fc824e1609fe81c451ebf00
mega123-art <agrawalparth326@gmail.com>
2025-06-01 02:14:22 +0530

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   node_modules/.package-lock.json
    	modified:   package-lock.json
    	modified:   package.json
    	modified:   src/models/user.models.js

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,5 +1,6 @@
 import mongoose, { Schema } from "mongoose";
-
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 const userSchema = new Schema(
   {
     username: {
@@ -40,11 +41,32 @@ const userSchema = new Schema(
       type: String,
       required: [true, "password is required!!"],
     },
-    refreshToken:{
-        type:String
-    }
+    refreshToken: {
+      type: String,
+    },
   },
   { timestamps: true }
 );
+userSchema.pre("save", async function (next) {
+  if (!this.modified("password")) return next();
+  this.password = bcrypt.hash(this.password, 10);
+  next();
+});
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
 
+userSchema.methods.generateAccessToken = function () {
+  //short lived token
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+      fullname: this.fullname,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    { expiresIn: ACCESS_TOKEN_EXPIRY }
+  );
+};
 export const User = mongoose.model("User", userSchema);
